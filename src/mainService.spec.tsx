f3812bc48ded460642119b779f117bc7308cf855
Gregor Anders <gregor.anders@gmail.com>
2020-01-06 20:04:33 +0100

    #1 - WIP: Added more tests
    Signed-off-by: Gregor Anders <gregor.anders@gmail.com>

--- src/mainService.spec.tsx
+++ src/mainService.spec.tsx
@@ -9,4 +9,24 @@ describe("MainService", (): void => {
   it("MainService export exists", (): void => {
     expect(TestSubject.MainService).toBeDefined();
   });
+
+  it("register", (): void => {
+    const ipc: any = {
+      on: jest.fn(),
+    };
+
+    const testSubject: TestSubject.MainService = new TestSubject.MainService(ipc, "main-channel");
+    testSubject.register();
+    expect(ipc.on).toHaveBeenCalledTimes(1);
+  });
+
+  it("callback", (): void => {
+    const ipc: any = {
+      on: jest.fn(),
+    };
+
+    const testSubject: TestSubject.MainService = new TestSubject.MainService(ipc, "main-channel");
+    testSubject.register();
+    expect(ipc.on).toHaveBeenCalledTimes(1);
+  });
 });
